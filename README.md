956942ccf963691831f6c99d7edd658f7a615ffe
Johan Berdat <jojolebarjos@gmail.com>
2021-12-29 14:26:33 +0100

    Clean code

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # Jigsaw generator
 
-...
+This is a small experiment where jigsaw puzzles are generated.
 
-
-## A bit of theory
-
-A piece has 4 edges, each edge having a so-called type.
+We focus on regular grids, where all pieces are squares of the same size.
+A piece has 4 edges, selected from a small set of shapes, namely edge types.
 By design, adjacent pieces must have complementary edge types.
+
 Let us distinguish between two families of edge types:
 
  1. Symmetric edges, which are their own complement.
@@ -15,59 +14,128 @@ Let us distinguish between two families of edge types:
  2. Asymmetric edges, which come in pairs.
     Traditionally, most asymmetric edges are designed in a male-female fashion, where one edge has some extrusion.
 
+...
 
-### Number of unique pieces
+
+## Getting started
+
+...
+
+```
+g++ -O3 -std=c++11 -o unique unique.cpp
+g++ -O3 -std=c++11 -o generate generate.cpp
+```
+
+...
+
+```
+./unique
+```
+
+...
+
+```
+./generate > jigsaw.jl
+python3 print.py
+```
+
+
+## Number of unique pieces
 
 Regardless of whether a jigsaw puzzle can be solved, we can estimate how many different pieces can be uniquely created, given the number of edge types.
+We distinguish between the following criteria:
 
-| q  | p     |
-|----|-------|
-| 1  | 1     |
-| 2  | 6     |
-| 3  | 24    |
-| 4  | 70    |
-| 5  | 165   |
-| 6  | 336   |
-| 7  | 616   |
-| 8  | 1044  |
-| 9  | 1665  |
-| 10 | 2530  |
-| 11 | 3696  |
-| 12 | 5226  |
-| 13 | 7189  |
-| 14 | 9660  |
-| 15 | 12720 |
-| 16 | 16456 |
-| 17 | 20961 |
-| 18 | 26334 |
-| 19 | 32680 |
-| 20 | 40110 |
+ * All pieces, where rotated pieces are considered as distinct;
+ * Canonical pieces, where rotated copies are deduplicated (i.e. how usual jigsaw puzzles are designed);
+ * Canonical pieces with symmetrical pieces removed (to ensure uniqueness in the case of apictural jigsaws);
+ * Canonical pieces with symmetrical pieces removed, but also where flipped copies are deduplicated (i.e. when there is no way to distinguish between front and back faces).
+
+|edges|all|canonical|canonical&asymmetric|canonical&asymmetric&flipped|
+|---|---|---|---|---|
+|1|1|1|0|0|
+|2|16|6|3|3|
+|3|81|24|18|15|
+|4|256|70|60|45|
+|5|625|165|150|105|
+|6|1296|336|315|210|
+|7|2401|616|588|378|
+|8|4096|1044|1008|630|
+|9|6561|1665|1620|990|
+|10|10000|2530|2475|1485|
+|11|14641|3696|3630|2145|
+|12|20736|5226|5148|3003|
+|13|28561|7189|7098|4095|
+|14|38416|9660|9555|5460|
+|15|50625|12720|12600|7140|
+|16|65536|16456|16320|9180|
+|17|83521|20961|20808|11628|
+|18|104976|26334|26163|14535|
+|19|130321|32680|32490|17955|
+|20|160000|40110|39900|21945|
+
+Note that given any valid jigsaw, one can trivially generate additional variants by swapping swapping edge types (i.e. re-assign colors) or flip the whole board (i.e. axial symmetry).
+
+
+## Examples
+
+Using the provided code, we focus on jigsaw puzzles that have a single solution (excluding the rotation of the whole board).
+
+
+### 3x3
+
+```json
+[[[1, 0, 0, 0], [1, 0, 2, 2], [0, 0, 2, 1]],
+ [[1, 0, 0, 1], [1, 1, 2, 2], [0, 2, 2, 2]],
+ [[2, 2, 0, 0], [1, 1, 1, 0], [0, 1, 2, 0]]]
+```
+
+With disambiguation nudge at the bottom:
+
+```json
+[[[1, 0, 0, 1], [1, 0, 2, 1], [0, 0, 2, 1]],
+ [[1, 2, 0, 1], [1, 2, 2, 2], [0, 2, 2, 2]],
+ [[2, 2, 0, 0], [1, 1, 1, 2], [0, 1, 2, 0]]]
+```
+
+
+### 4x4
+
+```json
+[[[1, 0, 0, 1], [4, 0, 2, 4], [4, 0, 3, 4], [0, 0, 3, 2]],
+ [[2, 2, 0, 1], [4, 3, 1, 2], [1, 3, 3, 3], [0, 1, 2, 0]],
+ [[3, 2, 0, 4], [4, 1, 4, 3], [0, 4, 3, 3], [0, 0, 0, 1]],
+ [[3, 3, 0, 0], [4, 4, 4, 0], [1, 4, 3, 0], [0, 2, 2, 0]]]
+```
+
+
+### 5x5
+
+```json
+[[[3, 0, 0, 1], [1, 0, 4, 3], [2, 0, 2, 3], [1, 0, 1, 2], [0, 0, 2, 2]],
+ [[1, 2, 0, 3], [1, 4, 2, 3], [1, 4, 2, 1], [3, 1, 2, 3], [0, 1, 4, 2]],
+ [[4, 4, 0, 3], [3, 4, 3, 3], [2, 2, 4, 2], [2, 4, 1, 1], [0, 1, 1, 1]],
+ [[2, 4, 0, 1], [4, 4, 1, 1], [2, 1, 3, 3], [3, 2, 1, 4], [0, 2, 4, 3]],
+ [[3, 2, 0, 0], [3, 2, 4, 0], [2, 4, 4, 0], [3, 3, 1, 0], [0, 4, 4, 0]]]
+```
+
+Some inner borders allowed:
+
+```json
+[[[2, 0, 0, 1], [3, 0, 1, 3], [1, 0, 4, 3], [4, 0, 2, 3], [0, 0, 3, 2]],
+ [[0, 2, 0, 0], [2, 4, 0, 2], [2, 4, 1, 2], [4, 4, 1, 4], [0, 1, 3, 1]],
+ [[3, 0, 0, 4], [2, 1, 4, 4], [2, 1, 1, 4], [4, 3, 1, 1], [0, 2, 3, 3]],
+ [[0, 3, 0, 4], [2, 3, 0, 2], [1, 3, 1, 2], [4, 2, 2, 2], [0, 4, 3, 3]],
+ [[3, 3, 0, 0], [2, 1, 4, 0], [2, 1, 1, 0], [3, 1, 1, 0], [0, 4, 4, 0]]]
+```
 
 
 ## References
 
-No easy puzzles: Hardness results for jigsaw puzzles
-https://www.sciencedirect.com/science/article/pii/S0304397515001607
-
-Solving Small-piece Jigsaw Puzzles by Growing Consensus
-https://openaccess.thecvf.com/content_cvpr_2016/papers/Son_Solving_Small-Piece_Jigsaw_CVPR_2016_paper.pdf
-
-
-
-Shotgun Assembly of Labeled Graphs
-https://ieeexplore.ieee.org/abstract/document/8119552
-
-Solving jigsaw puzzles by computer
-https://link.springer.com/article/10.1007%252FBF02186360
-
-Automatic Reassembly of Three-Dimensional Jigsaw Puzzles
-https://www.worldscientific.com/doi/abs/10.1142/S0219467816500091
-
-Automatic Solution of Jigsaw Puzzles
-https://link.springer.com/article/10.1007/s10851-013-0454-3
-
-Multiphase Numerical Modeling of Dendritic Solidification for Jigsaw Puzzle Generation
-https://n-e-r-v-o-u-s.com/works/puzzles/jigsaw-paper.pdf
-
-Jigsaw puzzle images for steganography
-https://www.spiedigitallibrary.org/journals/Optical-Engineering/volume-48/issue-7/077006/Jigsaw-puzzle-images-for-steganography/10.1117/1.3159872.short?SSO=1
+ * No easy puzzles: Hardness results for jigsaw puzzles ([link](https://www.sciencedirect.com/science/article/pii/S0304397515001607))
+ * Solving Small-piece Jigsaw Puzzles by Growing Consensus ([link](https://openaccess.thecvf.com/content_cvpr_2016/papers/Son_Solving_Small-Piece_Jigsaw_CVPR_2016_paper.pdf))
+ * Shotgun Assembly of Labeled Graphs ([link](https://ieeexplore.ieee.org/abstract/document/8119552))
+ * Solving jigsaw puzzles by computer ([link](https://link.springer.com/article/10.1007%252FBF02186360))
+ * Automatic Reassembly of Three-Dimensional Jigsaw Puzzles ([link](https://www.worldscientific.com/doi/abs/10.1142/S0219467816500091))
+ * Automatic Solution of Jigsaw Puzzles ([link](https://link.springer.com/article/10.1007/s10851-013-0454-3))
+ * Multiphase Numerical Modeling of Dendritic Solidification for Jigsaw Puzzle Generation ([link](https://n-e-r-v-o-u-s.com/works/puzzles/jigsaw-paper.pdf))
+ * Jigsaw puzzle images for steganography ([link](https://www.spiedigitallibrary.org/journals/Optical-Engineering/volume-48/issue-7/077006/Jigsaw-puzzle-images-for-steganography/10.1117/1.3159872.short?SSO=1))
--- src/generate.cpp
+++ src/generate.cpp
@@ -1,37 +1,42 @@
 
 #include <algorithm>
+#include <chrono>
 #include <cstdio>
 #include <cstdint>
 #include <cstdlib>
 #include <map>
 
 
+// Size of the grid
+// Note: exhaustive search has exponential complexity!
 constexpr unsigned WIDTH = 4;
 constexpr unsigned HEIGHT = 4;
 
-/*
-constexpr unsigned BITS = 2;
-constexpr unsigned MASK = (1 << BITS) - 1;
-constexpr unsigned NUM = (1 << BITS) * (1 << BITS) * (1 << BITS) * (1 << BITS);
-constexpr unsigned TYPES = 3;
-typedef uint8_t Piece;
-*/
+// How many edge types
+// Note: in this version, type 0 is symmetric, remaining types comes in pair (i.e. male-female);
+//       however, you can edit how adjacency constraints are defined, later in the code
+constexpr unsigned TYPES = 5;
 
+// How many bits are needed to store these types
 constexpr unsigned BITS = 3;
 constexpr unsigned MASK = (1 << BITS) - 1;
 constexpr unsigned NUM = (1 << BITS) * (1 << BITS) * (1 << BITS) * (1 << BITS);
-constexpr unsigned TYPES = 5;
 typedef uint16_t Piece;
 
 
+// Some tables are pre-computed and used globally
 Piece canonical[NUM];
 bool right[NUM][NUM];
 bool below[NUM][NUM];
 
 
-
+// Solution container, used to group solution by their piece set
 struct Solution {
+
+    // The actual solution
     Piece stack[WIDTH * HEIGHT];
+
+    // Associated canonical set of pieces
     Piece id[WIDTH * HEIGHT];
 
     Solution(Piece s[]) {
@@ -41,20 +46,19 @@ struct Solution {
         }
         std::sort(id, id + WIDTH * HEIGHT);
     }
-    
+
     bool operator<(Solution const & other) const {
         return std::lexicographical_compare(id, id + WIDTH * HEIGHT, other.id, other.id + WIDTH * HEIGHT);
     }
 };
 
 
-
-
+// Exhaustive search of the given set of pieces
+// Note: pieces are placed one-by-one, starting from upper-left corner and growing row-by-row
 template <typename Callback>
 void search(bool has[], Callback & callback) {
 
     // Recurse, using stack for efficiency
-    uint64_t total = 0;
     Piece stack[WIDTH * HEIGHT];
     unsigned index = 0;
     stack[0] = 0;
@@ -68,28 +72,25 @@ check:
         bool fit = true;
         unsigned x = index % WIDTH;
         unsigned y = index / WIDTH;
-        if (x == 0) {
-            if ((piece >> (2 * BITS) & MASK) != 0)
-                fit = false;
-        } else {
-            if (x == WIDTH - 1) {
-                if ((piece & MASK) != 0)
-                    fit = false;
-            }
-            if (!right[stack[index - 1]][piece])
-                fit = false;
-        }
-        if (y == 0) {
-            if ((piece >> BITS & MASK) != 0)
-                fit = false;
-        } else {
-            if (y == HEIGHT - 1) {
-                if ((piece >> (3 * BITS) & MASK) != 0)
-                    fit = false;
-            }
-            if (!below[stack[index - WIDTH]][piece])
-                fit = false;
-        }
+
+        // An edge is flat if and only if it is a border
+        if (!(
+            (x == WIDTH  - 1) == ((piece >> (0 * BITS) & MASK) == 0) &&
+            (y ==          0) == ((piece >> (1 * BITS) & MASK) == 0) &&
+            (x ==          0) == ((piece >> (2 * BITS) & MASK) == 0) &&
+            (y == HEIGHT - 1) == ((piece >> (3 * BITS) & MASK) == 0)
+        ))
+            fit = false;
+
+        // Edge must match piece on the left side
+        if (x > 0 && !right[stack[index - 1]][piece])
+            fit = false;
+
+        // Edge must match piece above
+        if (y > 0 && !below[stack[index - WIDTH]][piece])
+            fit = false;
+
+        // We can place it
         if (fit) {
 
             // Is jigsaw complete?
@@ -98,6 +99,8 @@ check:
                     return;
                 goto inc;
             }
+
+            // Otherwise, push piece on stack
             has[canonical[stack[index]]] = false;
             ++index;
             stack[index] = 0;
@@ -105,23 +108,27 @@ check:
         }
     }
 
-    // Increment current depth
+    // Check whether this depth is exhausted
 inc:
     if (stack[index] == NUM - 1) {
+
+        // That was the highest level, search is therefore complete
         if (index == 0)
-            goto done;
+            return;
+
+        // Pop from stack
         --index;
         has[canonical[stack[index]]] = true;
         goto inc;
     }
+
+    // Print a dot each time the highest level of the loop is incremented, so that we see how much time we need
     if (index == 0)
         putc('.', stderr);
+
+    // Increment piece
     ++stack[index];
     goto check;
-
-    // Search complete
-done:
-    ;
 }
 
 
@@ -138,15 +145,17 @@ bool print(Piece const stack[]) {
 }
 
 
+// Collected data
+uint64_t total = 0;
 std::map<Solution, uint64_t> counter;
 
 bool collect(Piece stack[]) {
-    static uint64_t k = 0;
     Solution solution(stack);
+    ++total;
     ++counter[solution];
     /*
-    if (++k % 100000 == 0 || true) {
-        fprintf(stderr, "%llu (%zu)\n", k, counter.size());
+    if (total % 100000 == 0 || true) {
+        fprintf(stderr, "%llu (%zu)\n", total, counter.size());
         for (unsigned i = 0; i < WIDTH; ++i)
             fprintf(stderr, "  %u", solution.stack[i]);
         fprintf(stderr, "\n");
@@ -156,6 +165,10 @@ bool collect(Piece stack[]) {
 
 
 int main(int argc, char* argv[]) {
+
+    //
+    // PRECOMPUTED TABLES
+    //
 
     // Generate mapping to unique pieces
     for (unsigned p = 0; p < NUM; ++p) {
@@ -174,11 +187,23 @@ int main(int argc, char* argv[]) {
         for (unsigned b = 0; b < NUM; ++b) {
             unsigned a_right = a & MASK;
             unsigned b_left = b >> (2 * BITS) & MASK;
-            right[a][b] = (a_right == 0 && b_left == 0) || (a_right > 0 && a_right % 2 == 0 && b_left == a_right - 1) || (a_right > 0 && a_right % 2 == 1 && b_left == a_right + 1);
             unsigned a_below = a >> (3 * BITS) & MASK;
             unsigned b_above = b >> BITS & MASK;
-            below[a][b] = (a_below == 0 && b_above == 0) || (a_below > 0 && a_below % 2 == 0 && b_above == a_below - 1) || (a_below > 0 && a_below % 2 == 1 && b_above == a_below + 1);
+
+            // Edge rules are implemented here
+            //  * Type 0 is symmetric (e.g. flat) and therefore only matches itself
+            //  * Type 1&2 are asymmetric (e.g. male-female) and always come in pair
+            right[a][b] = (a_right == 0 && b_left == 0)
+                       || (a_right > 0 && a_right % 2 == 0 && b_left == a_right - 1)
+                       || (a_right > 0 && a_right % 2 == 1 && b_left == a_right + 1);
+            below[a][b] = (a_below == 0 && b_above == 0)
+                       || (a_below > 0 && a_below % 2 == 0 && b_above == a_below - 1)
+                       || (a_below > 0 && a_below % 2 == 1 && b_above == a_below + 1);
         }
+
+    //
+    // PIECE SET
+    //
 
     // We have one of each (canonical, i.e. unique) piece
     bool has[NUM] = {};
@@ -191,16 +216,24 @@ int main(int argc, char* argv[]) {
             has[i | j << BITS | i << (2 * BITS) | j << (3 * BITS)] = false;
 
     // Some edge types do not exist
+    // Note: depending on the configuration, there may be a lot of non-existent pieces!
     for (unsigned p = 0; p < NUM; ++p)
         for (int i = 0; i < 4; ++i)
             if ((p >> (BITS * i) & MASK) >= TYPES)
                 has[p] = false;
 
+    // Drop unwanted edge pieces (i.e. the ones with 3 flat edges) 
+    for (unsigned i = 0; i < TYPES; ++i)
+        has[canonical[i]] = false;
+
     // Drop some pieces randomly
+    // Note: this is needed to solve in an acceptable amount of time, but this is tricky to fine-tune...
+    //  * Too few pieces means no solution
+    //  * Too many pieces means an insane amount of time
     srand(1337);
     for (unsigned p = 0; p < NUM; ++p)
         if (has[p])
-            has[p] = rand() < RAND_MAX / 5;
+            has[p] = rand() < RAND_MAX / 4;
 
     // Print available pieces
     unsigned count = 0;
@@ -210,23 +243,30 @@ int main(int argc, char* argv[]) {
             fprintf(stderr, "%02u) %u, %u, %u, %u -> %u\n", count, p & MASK, p >> BITS & MASK, p >> (2 * BITS) & MASK, p >> (3 * BITS) & MASK, p);
         }
 
+    // Do the search
+    // Note: one could also print solutions directly, but there are usually too many of them
+    auto start = std::chrono::high_resolution_clock::now();
     //search(has, print);
     search(has, collect);
+    auto end = std::chrono::high_resolution_clock::now();
+    fprintf(stderr, "\n");
 
-    fprintf(stderr, "%zu selections.\n", counter.size());
+    // Done
+    std::chrono::duration<float> duration = end - start;
+    fprintf(stderr, "%llu solutions, grouped in %zu sets, in %.03f seconds.\n", total, counter.size(), duration.count());
 
-    uint64_t minimum = -1;
-    for (auto & it : counter) {
-        minimum = std::min(minimum, it.second);
-    }
-
-    fprintf(stderr, "Best selections have %llu solutions\n", minimum);
-    for (auto & it : counter) 
-        if (it.second == minimum) {
-            //printf("%llu ", it.second);
-            print(it.first.stack);
+    // Report only the solutions that have the smallest number of combination
+    // Note: a jigsaw with unique solution usually has 4 of them, given that the whole board can be rotated
+    if (counter.size() > 0) {
+        uint64_t minimum = -1;
+        for (auto & it : counter) {
+            minimum = std::min(minimum, it.second);
         }
+        fprintf(stderr, "Best selections have %llu solutions\n", minimum);
+        for (auto & it : counter) 
+            if (it.second == minimum)
+                print(it.first.stack);
+    }
 
     return 0;
 }
-
