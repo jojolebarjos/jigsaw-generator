3f651e388b5bc8270c2f062a375e2d87a20fac0c
Johan Berdat <jojolebarjos@gmail.com>
2022-04-05 22:34:50 +0200

    Toward fine-tuning new circle design

--- plus/svg.py
+++ plus/svg.py
@@ -38,9 +38,8 @@ class Type(IntEnum):
 
 
 class Buffer:
-    def __init__(self, style="original", offset=0.2):
+    def __init__(self, style="original"):
         self.style = style
-        self.offset = offset
         self.data = []
         self.height = -1
         self.width = -1
@@ -49,8 +48,8 @@ class Buffer:
         a = origin + forward
         self.data.append("L {} {}".format(*a))
 
-    def circular_hook_segment(self, origin, forward, right, unit=0.35):
-        offset = -self.offset * unit
+    def original_hook_segment(self, origin, forward, right, unit=0.35):
+        offset = -0.2 * unit
 
         # Control points
         k = np.sqrt(2)
@@ -93,8 +92,50 @@ class Buffer:
             q2 = q1 + right * offset
             self.data.append("C {} {}, {} {}, {} {}".format(*q2, *q1, *e))
 
+    def circle_hook_segment(self, origin, forward, right, unit, offset=0.0, shift=0.0):
+
+        # Control points
+        k = np.sqrt(2)
+        a = origin + forward * (0.5 + shift - 0.4 * unit) + right * offset
+        b = origin + forward * (0.5 + shift - 0.4 / k * unit) + right * (0.6 * unit - 0.4 / k * unit + offset)
+        c = origin + forward * (0.5 + shift + 0.4 / k * unit) + right * (0.6 * unit - 0.4 / k * unit + offset)
+        d = origin + forward * (0.5 + shift + 0.4 * unit) + right * offset
+        e = origin + forward
+
+        # Radii
+        r1 = 0.2 * unit
+        r2 = 0.4 * unit
+
+        # Which side to choose
+        cross = forward[0] * right[1] - forward[1] * right[0]
+        is_system_mirrored = cross < 0
+        if is_system_mirrored:
+            sweep = 0
+        else:
+            sweep = 1
+
+        # Slope to hook
+        if offset == 0:
+            self.data.append("L {} {}".format(*a))
+        else:
+            q1 = origin + forward * (0.25 + shift * 0.5 - 0.2 * unit)
+            q2 = q1 + right * offset
+            self.data.append("C {} {}, {} {}, {} {}".format(*q1, *q2, *a))
+
+        # Draw hook
+        self.data.append("A {} {} 0 0 {} {} {}".format(r1, r1, sweep, *b))
+        self.data.append("A {} {} 0 1 {} {} {}".format(r2, r2, 1 - sweep, *c))
+        self.data.append("A {} {} 0 0 {} {} {}".format(r1, r1, sweep, *d))
+
+        # Slope back to corner
+        if offset == 0:
+            self.data.append("L {} {}".format(*e))
+        else:
+            q1 = origin + forward * (0.75 + shift * 0.5 + 0.2 * unit)
+            q2 = q1 + right * offset
+            self.data.append("C {} {}, {} {}, {} {}".format(*q2, *q1, *e))
+
     def capsule_hook_segment(self, origin, forward, right, inner_radius, outer_radius, length, shift=0.0):
-        # TODO also use offset?
 
         # Control points
         a = origin + forward * (0.5 - 0.5 * length + shift)
@@ -123,7 +164,6 @@ class Buffer:
         self.data.append("L {} {}".format(*g))
 
     def double_capsule_hook_segment(self, origin, forward, right, inner_radius, outer_radius, length, bias, twisted=False):
-        # TODO also use offset?
 
         # Control points
         twisted_right = -right if twisted else right
@@ -172,16 +212,40 @@ class Buffer:
             self.flat_segment(origin, forward)
             return
 
-        # Circular-ish hooks
+        # The old circular-ish hooks
         if self.style == "original":
             if type % 2 == 0:
                 right = -right
             index = (type - 1) // 2
+            unit = [0.45, 0.35, 0.25][index % 3]
+            offset = -0.2 * unit
 
             # Centered
             if 0 <= index < 3:
-                unit = [0.45, 0.35, 0.25][index]
-                self.circular_hook_segment(origin, forward, right, unit)
+                self.original_hook_segment(origin, forward, right, unit, offset)
+                return
+
+        # Circular-ish hooks
+        if self.style == "circle":
+            if type % 2 == 0:
+                right = -right
+            index = (type - 1) // 2
+            unit = [0.45, 0.35, 0.25][index % 3]
+            offset = -0.2 * unit
+
+            # Centered
+            if 0 <= index < 3:
+                self.circle_hook_segment(origin, forward, right, unit, offset)
+                return
+
+            # Right
+            if 3 <= index < 6:
+                self.circle_hook_segment(origin, forward, right, unit, offset, 0.15)
+                return
+
+            # Left
+            if 6 <= index < 9:
+                self.circle_hook_segment(origin, forward, right, unit, offset, -0.15)
                 return
 
         # Capsule-like hooks
@@ -285,11 +349,13 @@ if __name__ == "__main__":
     #matrix = [[[0, 0, 0, 4], [3, 0, 0, 4], [0, 0, 4, 3]], [[3, 3, 0, 3], [3, 3, 4, 4], [0, 4, 4, 4]], [[4, 4, 0, 0], [4, 3, 3, 0], [0, 3, 3, 0]]]
     #matrix = [[[1, 0, 0, 4], [1, 0, 2, 1], [4, 0, 2, 4], [3, 0, 3, 4], [0, 0, 4, 5]], [[1, 3, 0, 3], [0, 2, 2, 1], [6, 3, 0, 0], [4, 3, 5, 2], [0, 6, 3, 5]], [[1, 4, 0, 5], [1, 2, 2, 3], [5, 0, 2, 5], [2, 1, 6, 5], [0, 6, 1, 3]], [[1, 6, 0, 2], [5, 4, 2, 1], [3, 6, 6, 5], [3, 6, 4, 5], [0, 4, 4, 1]], [[6, 1, 0, 0], [3, 2, 5, 0], [6, 6, 4, 0], [5, 6, 5, 0], [0, 2, 6, 0]]]
     #matrix = [[[6, 0, 0, 5], [4, 0, 5, 3], [5, 0, 3, 6], [0, 0, 6, 3]], [[3, 6, 0, 5], [4, 4, 4, 4], [5, 5, 3, 6], [0, 4, 6, 4]], [[5, 6, 0, 6], [4, 3, 6, 3], [4, 5, 3, 3], [0, 3, 3, 3]], [[4, 5, 0, 0], [3, 4, 3, 0], [4, 4, 4, 0], [0, 4, 3, 0]]]
-    matrix = [[[2, 0, 0, 18], [1, 0, 1, 18], [17, 0, 2, 1], [0, 0, 12, 17]], [[24, 11, 0, 2], [24, 11, 23, 29], [12, 2, 23, 17], [0, 12, 17, 12]], [[18, 1, 0, 11], [30, 29, 11, 24], [23, 12, 30, 17], [0, 17, 24, 30]], [[29, 18, 0, 0], [6, 23, 29, 0], [23, 12, 5, 0], [0, 30, 24, 0]]]
+    #matrix = [[[2, 0, 0, 18], [1, 0, 1, 18], [17, 0, 2, 1], [0, 0, 12, 17]], [[24, 11, 0, 2], [24, 11, 23, 29], [12, 2, 23, 17], [0, 12, 17, 12]], [[18, 1, 0, 11], [30, 29, 11, 24], [23, 12, 30, 17], [0, 17, 24, 30]], [[29, 18, 0, 0], [6, 23, 29, 0], [23, 12, 5, 0], [0, 30, 24, 0]]]
+    matrix = [[[12, 0, 0, 11], [12, 0, 17, 6], [4, 0, 17, 5], [0, 0, 3, 4]], [[11, 18, 0, 11], [6, 5, 18, 5], [11, 6, 5, 11], [0, 3, 18, 5]], [[17, 18, 0, 5], [18, 6, 12, 5], [4, 18, 11, 11], [0, 6, 3, 11]], [[12, 6, 0, 0], [11, 6, 17, 0], [5, 18, 18, 0], [0, 18, 6, 0]]]
     matrix = np.array(matrix)
 
     #style = "original"
-    style = "capsule"
+    style = "circle"
+    #style = "capsule"
     buffer = Buffer(style=style)
     buffer.jigsaw(matrix)
 
